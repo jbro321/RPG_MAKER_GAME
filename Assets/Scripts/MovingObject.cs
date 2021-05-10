79805141abc1b53a402e29f4cc8a85196d8fd440
jbro321 <jaehyung0321@gmail.com>
2021-05-10 23:24:43 +0900

    210510

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -5,14 +5,70 @@ using UnityEngine;
 public class MovingObject : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public float speed;
+
+    private Vector3 vector;
+    public float runSpeed;
+    private float applyRunSpeed;
+    private bool applyRunFlag = false;
+
+    public int walkCount;
+    private int currentWalkCount;
+
+    private bool canMove = true;
+
+    // SpriteRenderer = 2.4, walkCount = 20
+    // 2.4 * 20 = 48 픽셀만큼 이동하고
+    // while
+    // currentWalkCount += 1 , 20
     void Start()
     {
         
     }
 
+    IEnumerator MoveCoroutine()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            applyRunSpeed = runSpeed;
+            applyRunFlag = true;
+        }
+        else
+            applyRunSpeed = 0;
+            applyRunFlag = false;
+
+        vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+        while (currentWalkCount < walkCount)
+        {
+            if(vector.x != 0)
+            {
+                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+            }
+            else if(vector.y != 0)
+            {
+                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+            }
+            if(applyRunFlag)
+                currentWalkCount++;
+            currentWalkCount++;
+            yield return new WaitForSeconds(0.01f);
+        }
+        currentWalkCount = 0;
+        canMove = true;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if(canMove)
+        {
+            if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            {
+                canMove = false;
+                StartCoroutine(MoveCoroutine());
+            }
+        }
     }
 }
